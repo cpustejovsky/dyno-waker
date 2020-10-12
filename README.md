9b75d03922098d3f941e9e5af9e247e8b029e32f
cpustejovsky <charles.pustejovsky@gmail.com>
2020-10-12 14:17:26 -0400

    set up types and variables for wake times; small addition to README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dyno-waker
+
+Rebuilding my dyno waker in Golang to be more easily integrated into the rebuild Estuary server.
--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-//set up times for dyno waker to fire (6am to 9pm EST Monday-Friday)
+//set up times for dyno waker to fire (6am to 9pm EST)
+type Hours []int
+type WakeTime struct {
+	AM Hours
+	PM Hours
+}
+
+var amHours = Hours{6, 7, 8, 9, 10, 11}
+var pmHours = Hours{12, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+var wt = WakeTime{amHours, pmHours}
 
 //determine whether it's the correct time to fire
+func isWakeTime(t time.Time) bool {
+	loc, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	t = t.In(loc)
+	fmt.Println(t.Location(), t.Format("15:04"))
+	return false
+}
+
 
 //wake function
+func wake() {}
 
 //use goroutines and channels and loop through (use twitter-bot for reference)
 
 func main() {
-	fmt.Println("Hello, world!")
+	ok := isWakeTime(time.Now())
+	if ok {
+		fmt.Print("Correct time to wake dynos!")
+	}
 }
