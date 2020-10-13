59f9059d423cc4c3df1bf601aa422400dc335c5e
cpustejovsky <charles.pustejovsky@gmail.com>
2020-10-13 14:18:50 -0400

    it works but it doesn't loop

--- main.go
+++ main.go
@@ -6,13 +6,11 @@ import (
 	"time"
 )
 
-//set up times for dyno waker to fire (6am to 9pm EST)
 type Hours []int
 
 var wakeHours = Hours{6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21}
 var halfHour time.Duration = 5 * time.Second
 
-//determine whether it's the correct time to fire
 func isWakeTime(t time.Time) bool {
 	loc, err := time.LoadLocation("America/New_York")
 	if err != nil {
@@ -27,33 +25,35 @@ func isWakeTime(t time.Time) bool {
 	return false
 }
 
-//wake function
-func wake(url string, c chan string) {
+//Wake takes a url as a string and a channel string. It checks if it is between 0600 and 1800 hours.
+//If it is not the correct time, it waits half an hour before checking again. If it is the correct time, it gets the url and then waits half an hour
+//If there was an error, it tries again after five seconds
+func Wake(url string, c chan string) {
 	ok := isWakeTime(time.Now())
 	if ok {
 		_, err := http.Get(url)
 		if err != nil {
 			fmt.Println("failed to hit", url)
 			time.Sleep(5 * time.Second)
-			wake(url, c)
+			go Wake(url, c)
 		}
 		fmt.Println("successfully hit", url)
 		c <- url
+		time.Sleep(halfHour)
+		go Wake(url, c)
 	} else {
 		fmt.Println("not the correct time, sleeping for half an hour")
 		time.Sleep(halfHour)
-		wake(url, c)
+		go Wake(url, c)
 	}
 }
-
-//use goroutines and channels and loop through (use twitter-bot for reference)
 
 func main() {
 	n := []string{"life-together-calculator", "truthify"}
 	c := make(chan string)
 	for _, pre := range n {
 		url := "https://" + pre + ".herokuapp.com"
-		go wake(url, c)
+		go Wake(url, c)
 		var wokeDynos []string
 		wokeDynos = append(wokeDynos, <-c)
 	}
